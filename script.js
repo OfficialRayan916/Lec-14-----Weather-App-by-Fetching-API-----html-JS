76ec96d1438863c0d4dff945e51ae60a90bc563f
Rayan916 <officialdevrayan@gmail.com>
2026-05-06 20:44:05 +0500

    App by API Fetching

---
+++ script.js
@@ -0,0 +1,12 @@
+const getWeather = () => {
+    const city = document.getElementById('city').value;
+    alert(city);
+
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=bad2376a81d5489a88982043261204&q=${city}&aqi=no`)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.error(error);
+  })
+}
